8c5462ab3f3698e3422aeb56817d1baa3dac3aa6
GIM-YOUNG-JOO <explicit13@naver.com>
2025-02-18 23:08:53 +0900

    02-18

--- week4/object-api-demo.js
+++ week4/object-api-demo.js
@@ -40,6 +40,39 @@ app.post('/planet', (req, res) => {
     res.json("지구와 "+ req.body.name + " 사이의 거리는 " + db.get(req.body.name).distanceFromEarth + "km 입니다.");
 });
 
+app.delete('/planet/:name', (req, res) => {
+    const name = decodeURIComponent(req.params.name);
+
+    if (db.has(name)) {
+        db.delete(name);
+        res.json({ message: name + "이 삭제되었습니다",
+            planets: Object.fromEntries(db) });
+    } else {
+        res.json({ error: name + "은 없는 데이터입니다" });
+    }
+});
+
+app.delete('/planet', (req, res) => {
+    db.clear();
+    res.json({ message: "db가 초기화 되었습니다."});
+});
+
+app.put('/planet/:name', (req, res) => {
+    const name = decodeURIComponent(req.params.name);
+
+    // query string
+    const newDistance = parseInt(req.query.distanceFromEarth, 10);
+
+    if (db.has(name)) {
+        db.get(name).distanceFromEarth = newDistance;
+        res.json({
+            message: `지구와 ${name}의 거리가 ${newDistance}km로 수정되었습니다.`
+        });
+    } else {
+        res.json({ error: "은 없는 데이터입니다" });
+    }
+});
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
